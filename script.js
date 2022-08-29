fc0c11c6a4cc53e77983d4f78bfc320d7803d9eb
Kieran Savory <96089380+kieransavory@users.noreply.github.com>
2022-08-29 18:48:16 +0100

    Initial commit and initial testing
    As title describes - initial commit of setup files and testing for bugs in newly created navbar

---
+++ script.js
@@ -0,0 +1,20 @@
+function navPageOpen() {
+
+    let navPage = document.getElementById('navPage');
+    let navButton = document.getElementById('menuToggle');
+
+    navPage.classList.toggle('active');
+    navButton.style.backgroundColor = "black";
+    menuToggle.classList.toggle('active');
+}
+
+function navPageClose() {
+
+    let navPage = document.getElementById('navPage');
+    let navButton = document.getElementById('menuToggle');
+
+    navPage.classList.toggle('active');
+    navButton.style.backgroundColor = "black";
+    menuToggle.classList.toggle('active');
+
+}
